37769675366fcc22a1c4502846731ebafd46e5e1
Yoel Amram <yoel.amram@gmail.com>
2017-03-06 11:42:32 +0200

    some fixes to jsom transformer

--- src/main/scala/yamrcraft/etlight/transformers/JsonTransformer.scala
+++ src/main/scala/yamrcraft/etlight/transformers/JsonTransformer.scala
@@ -10,15 +10,18 @@ class JsonTransformer(config: Config) extends Transformer[Message[String]] {
   // config settings
   val timestampField = config.getString("timestamp-field")
   val timestampFieldFormat = config.getString("timestamp-field-format")
+  val defaultMessageType = config.getString("default-message-type")
+  val typeSelectionField = config.getString("message-type-selection-field")
 
   @throws(classOf[EtlException])
   override def transform(key: Array[Byte], msg: Array[Byte]): Message[String] = {
     try {
       val msgStr = toString(msg)
       val msgJson = Json.parse(msgStr)
+      val msgType: String = (msgJson \ typeSelectionField).asOpt[String].getOrElse(defaultMessageType)
       val timestamp: Option[String] = (msgJson \ timestampField).asOpt[String]
       timestamp match {
-        case Some(event) => Message(msgStr, "AuditEvent", TimeUtils.stringTimeToLong(event, timestampFieldFormat))
+        case Some(event) => Message(msgStr, msgType, TimeUtils.stringTimeToLong(event, timestampFieldFormat))
         case None => throw new EtlException(ErrorType.TransformationError)
       }
     } catch {
